1a696e17f0b54545924a5b4e85eaad09c5a67170
Massimo Costa <costa.massimo@gmail.com>
2023-05-30 17:52:06 +0200

    feat: use templates for txt and csv

--- exporter/renderer.go
+++ exporter/renderer.go
@@ -1,16 +1,13 @@
 package exporter
 
 import (
-	_ "embed"
-	"encoding/csv"
 	"encoding/json"
-	"fmt"
-	"html/template"
+	html "html/template"
 	"io"
 	"io/fs"
 	"os"
 	"sort"
-	txttemplate "text/template"
+	txt "text/template"
 )
 
 type renderer interface {
@@ -20,68 +17,51 @@ type renderer interface {
 func newRenderer(format string, templatesFS fs.FS) renderer {
 	var r renderer
 	switch format {
-	case "text":
-		r = &consoleRenderer{}
-
 	case "csv":
-		r = &csvRenderer{}
+		r = &templateRenderer[*txt.Template]{
+			fs:           templatesFS,
+			templateName: "templates/csv.tmpl",
+			parseFunc:    txt.ParseFS,
+		}
 
 	case "html":
-		r = &templateRenderer[*template.Template]{
+		r = &templateRenderer[*html.Template]{
 			fs:           templatesFS,
-			templateName: "templates/output.html",
-			parseFunc:    template.ParseFS,
+			templateName: "templates/html.tmpl",
+			parseFunc:    html.ParseFS,
 		}
 
 	case "md":
-		r = &templateRenderer[*txttemplate.Template]{
+		r = &templateRenderer[*txt.Template]{
 			fs:           templatesFS,
-			templateName: "templates/output.md",
-			parseFunc:    txttemplate.ParseFS,
+			templateName: "templates/md.tmpl",
+			parseFunc:    txt.ParseFS,
 		}
 
 	case "json":
 		r = &jsonRenderer{}
 
 	default:
-		r = &consoleRenderer{}
+		r = &templateRenderer[*txt.Template]{
+			fs:           templatesFS,
+			templateName: "templates/text.tmpl",
+			parseFunc:    txt.ParseFS,
+		}
 	}
 	return r
 }
 
-type consoleRenderer struct{}
-
-func (r *consoleRenderer) Render(metaInfo data) error {
-	keys := make([]string, 0, len(metaInfo))
-	for k := range metaInfo {
-		keys = append(keys, k)
-	}
-	sort.Strings(keys)
-
-	for _, k := range keys {
-		fmt.Printf("%q:\n", k)
-		for _, item := range metaInfo[k] {
-			fmt.Printf("\tType: %q\n", item.Type)
-			fmt.Printf("\tHelp: %q\n", item.Help)
-			if item.Unit != "" {
-				fmt.Printf("\tUnit: %q\n", item.Unit)
-			}
-		}
-	}
-	return nil
-}
-
 type jsonRenderer struct{}
+
+type record struct {
+	Name string `json:"name,omitempty"`
+	Type string `json:"type,omitempty"`
+	Help string `json:"help,omitempty"`
+	Unit string `json:"unit,omitempty"`
+}
 
 func (r *jsonRenderer) Render(metaInfo data) error {
 	keys := sortedKeys(metaInfo)
-
-	type record struct {
-		Name string `json:"name,omitempty"`
-		Type string `json:"type,omitempty"`
-		Help string `json:"help,omitempty"`
-		Unit string `json:"unit,omitempty"`
-	}
 
 	records := make([]record, 0)
 	for _, k := range keys {
@@ -94,28 +74,11 @@ func (r *jsonRenderer) Render(metaInfo data) error {
 	return enc.Encode(records)
 }
 
-type csvRenderer struct{}
-
-func (r *csvRenderer) Render(metaInfo data) error {
-	keys := sortedKeys(metaInfo)
-
-	w := csv.NewWriter(os.Stdout)
-	w.Write([]string{"Name, Type, Help, Unit"})
-
-	for _, k := range keys {
-		for _, item := range metaInfo[k] {
-			w.Write([]string{k, item.Type, item.Help, item.Unit})
-		}
-	}
-	w.Flush()
-	return nil
-}
-
-type Executer interface {
+type Executor interface {
 	Execute(io.Writer, any) error
 }
 
-type templateRenderer[T Executer] struct {
+type templateRenderer[T Executor] struct {
 	fs           fs.FS
 	templateName string
 	parseFunc    func(fs.FS, ...string) (T, error)
@@ -127,13 +90,6 @@ func (r *templateRenderer[T]) Render(metaInfo data) error {
 	t, err := r.parseFunc(r.fs, r.templateName)
 	if err != nil {
 		return err
-	}
-
-	type record struct {
-		Name string
-		Type string
-		Help string
-		Unit string
 	}
 
 	records := make([]record, 0)
